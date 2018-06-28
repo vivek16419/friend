aa243f8bddeb0c7f1d0a4069ef5cdde556214e1e
vivek singh <200361499@student.georgianc.on.ca>
2018-06-28 18:04:26 -0400

    first commit

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,3 +1,15 @@
 exports. homepage = (req, res, next) =>{
     res.render('index', { title: 'Portfolio', message: 'hello world', });
   };
+
+  exports. project = (req, res, next) =>{
+    res.render('project', { title: 'Project', message: 'hello world', });
+  };
+
+  exports. service = (req, res, next) =>{
+    res.render('service', { title: 'service', message: 'hello world', });
+  };
+
+  exports. contact = (req, res, next) =>{
+    res.render('contact', { title: 'Contact', message: 'hello world', });
+  };
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,17 @@
 var express = require('express');
 var indexController = require('../controllers/indexController');
+var aboutController = require('../controllers/aboutController');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', indexController.homepage);
+
+router.get('/about', aboutController.about);
+
+router.get('/project',indexController.project);
+
+router.get('/service', indexController.service);
+
+router.get('/contact' ,indexController.contact);
 
 module.exports = router;
